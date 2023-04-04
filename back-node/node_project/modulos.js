8e762eb5aafe19c65b562cd6f0f326377d299c3d
Italo Neves <nevessrichardsonn@gmail.com>
2023-04-04 14:34:28 -0300

    linux

---
+++ back-node/node_project/modulos.js
@@ -0,0 +1,12 @@
+//colocando dentro de um objeto
+//modulo nativo
+const path = require ('path')
+
+console.log(path.basename(__filename))
+
+
+//meus modulos477
+
+const myModule = require('./exports')
+
+console.log(myModule)
